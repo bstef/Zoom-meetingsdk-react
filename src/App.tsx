6170bbd79947168645d6ff9cae1b0247fc4fd65c
Ben Bobby Stef <benjaminstef@gmail.com>
2026-01-16 12:12:49 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -70,7 +70,7 @@ function App() {
   return (
     <div className="App">
       <main>
-        <h1>Zoom Meeting SDK Sample React</h1>
+        <h1>Zoom Meeting on AWS Amplify</h1>
         <button onClick={getSignature}>Join Meeting</button>
       </main>
     </div>
